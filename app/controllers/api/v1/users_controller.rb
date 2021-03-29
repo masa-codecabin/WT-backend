6f168725487adb310902f14716e525749dab4c02
masafumikazama <masa@codecabin.com.au>
2021-03-29 16:24:48 +1000

    just start set up CRUD

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def index
+  end
 
+  def show
   end
 
   def create
-
   end
 
-  private
-
-  def user_params
-
+  def update
   end
+
+  def destroy
+  end
+
 end
